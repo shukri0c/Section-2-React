169dbefe82fca1dd5ed594db6f8c349e3643e977
shukri0c <shukriamahdi05@gmail.com>
2025-02-27 18:06:45 +0000

    Added a header to the page

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <img src = "src/images/globe.png" alt = "globe" />
+            <spam>my travel journal.</spam>
+        </header>
+    )
+
+
+}
